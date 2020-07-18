6d6ddace7d0e1ced68f2416203b5f6147b6c2821
Sjk-Lacoste <tmohlatlole@gmail.com>
2020-07-18 13:01:39 +0200

    Changed name to user field in Instructor models and made the image field optional for Skill model

--- courses/models.py
+++ courses/models.py
@@ -18,17 +18,17 @@ class Skill(models.Model):
     name = models.CharField(max_length=100)
     url = models.ManyToManyField(Link, blank=True)
     description = models.TextField()
-    image = models.ImageField(upload_to='images/skills/')
+    image = models.ImageField(upload_to='images/skills/', null=True)
 
     def __str__(self):
         return self.name
 
 class Instructor(models.Model):
-    name =models.ForeignKey(User, on_delete=models.CASCADE)
+    user =models.ForeignKey(User, on_delete=models.CASCADE)
     skills = models.ForeignKey(Skill, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name.username
+        return self.user.username
 
 class Category(models.Model):
     title = models.CharField(max_length=100)
