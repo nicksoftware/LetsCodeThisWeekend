c0cc969e8559b7978e8d6ea19573c6eafa0188d5
Sjk-Lacoste <tmohlatlole@gmail.com>
2020-07-18 11:37:54 +0200

    Modified models: Skill, Instructor, Tutorial, Category, Link, Course and Step

--- courses/admin.py
+++ courses/admin.py
@@ -1,16 +1,22 @@
 from django.contrib import admin
-from .models import Course,Step, Instructor, Category
+# from .models import (
+#     Skill,
+#     Course,
+#     Step,
+#     Instructor,
+#     Category
+# )
 
-class StepInline(admin.StackedInline):
-    model = Step
+# class StepInline(admin.StackedInline):
+#     model = Step
 
-class CourseAdmin(admin.ModelAdmin):
-    inlines = [StepInline,]
-    list_display = ('id', 'title','category', 'is_published', 'price', 'pub_date')
-    list_editable = ('is_published','category')
-    list_display_links = ('id', 'title')
+# class CourseAdmin(admin.ModelAdmin):
+#     inlines = [StepInline,]
+#     list_display = ('id', 'title','category', 'is_published', 'price', 'pub_date')
+#     list_editable = ('is_published','category')
+#     list_display_links = ('id', 'title')
 
-    list_per_page = 25
-admin.site.register(Instructor)
-admin.site.register(Category) 
-admin.site.register(Course,CourseAdmin)
+#     list_per_page = 25
+# admin.site.register(Instructor)
+# admin.site.register(Category)
+# admin.site.register(Course, CourseAdmin)
--- courses/models.py
+++ courses/models.py
@@ -1,50 +1,80 @@
+from django.conf import settings
 from django.db import models
 from embed_video.fields import EmbedVideoField
 
+User = settings.AUTH_USER_MODEL
+
 # Create your models here.
+class Link(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Skill(models.Model):
     name = models.CharField(max_length=100)
+    url = models.ManyToManyField(Link, blank=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/skills/')
+
+    def __str__(self):
+        return self.name
 
 class Instructor(models.Model):
-    name =models.ForeignKey('auth.User',on_delete=models.CASCADE)
+    name =models.ForeignKey(User, on_delete=models.CASCADE)
     skills = models.ForeignKey(Skill, on_delete=models.CASCADE)
-    bio = models.TextField()
-    
+
     def __str__(self):
         return self.name.username
 
 class Category(models.Model):
-    title = models.CharField(max_length=30,default="")
+    title = models.CharField(max_length=100)
     description = models.TextField()
+    image = models.ImageField(upload_to='images/categories/')
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name_plural = "categories"
+
 
 class Course(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.PROTECT,default="")
-    title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='images/%Y/%m/%d')
-    price = models.IntegerField(default=0)
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField()
-    pub_date =  models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/courses/')
+    publish_date =  models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+class Tutorial(models.Model):
+    title = models.CharField(max_length=100)
+    video = EmbedVideoField()
+    external_links = models.ManyToManyField(Link, related_name='link', blank=True)
+    created_at = models.DateField(auto_now_add=True)
+
     def __str__(self):
         return self.title
 
 class Step(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=100)
+    order = models.IntegerField(default=1)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    promo_video = EmbedVideoField()
+    tutorials = models.ManyToManyField(Tutorial, related_name='tutorial')
     description = models.TextField()
-    video = EmbedVideoField(default="")
-    
-    order = models.IntegerField(default=0)
-    Course = models.ForeignKey(Course,on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['order',]
 
     def __str__(self):
         return self.title
-
-
 
